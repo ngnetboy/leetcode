f9327f3a1c9181b80f8d7aa4d926af9ba921faf6
boy net <boynet@boydeMacBook-Pro.local>
2019-04-25 22:25:35 +0800

    leetcode 1,2.

---
+++ leetcode-1-sum.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+/*
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+*/
+
+func towSum(nums []int, target int) []int {
+	length := len(nums)
+	for i := 0; i < length; i++ {
+		for j := i + 1; j < length; j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+func towSum2(nums []int, target int) []int {
+	mapIndex := make(map[int]int, len(nums))
+
+	for index, val := range nums {
+		subVal := target - val
+		if mapI, ok := mapIndex[subVal]; ok {
+			return []int{mapI, index}
+		}
+		mapIndex[val] = index
+	}
+	return []int{}
+}
+
+func main() {
+	val := []int{2, 7, 11, 15}
+	result := towSum2(val, 9)
+	fmt.Println(result)
+}
---
+++ leetcode-2-sumList.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+/*
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，
+并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func initList(valList []int) *ListNode {
+	var head, l *ListNode
+	//head := l
+	for _, val := range valList {
+
+		tmp := new(ListNode)
+		tmp.Val = val
+		tmp.Next = nil
+
+		if l == nil {
+			head = tmp
+			l = tmp
+		} else {
+			l.Next = tmp
+			l = tmp
+		}
+	}
+	return head
+}
+
+func showList(node *ListNode) {
+	for l := node; l != nil; l = l.Next {
+		fmt.Println(l.Val)
+	}
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return nil
+}
+
+func main() {
+	firstList := initList([]int{2, 4, 3})
+	secondList := initList([]int{5, 6, 4})
+
+	showList(firstList)
+	showList(secondList)
+
+	result := addTwoNumbers(firstList, secondList)
+	showList(result)
+}
