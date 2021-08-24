d1b20d1a75bae9b8052a1e02e092d2e33dce6f67
yi li <yi.li@grabtaxi.com>
2021-08-24 18:52:47 +0800

    update pailiezuhe

--- pailiezuhe.go
+++ pailiezuhe.go
@@ -20,11 +20,33 @@ func dst2(nums []int, index int, p []int, res *[][]int, used *[]bool) {
 	}
 }
 
-func main() {
+func test1() {
 	num := []int{4, 2, 6}
 	p := make([]int, len(num))
 	res := [][]int{}
 	used := make([]bool, len(num))
 	dst2(num, 0, p, &res, &used)
 	fmt.Println(res)
+}
+
+func permutation(S string) []string {
+	if len(S) == 1 {
+		return []string{S}
+	}
+	// 与拼接得到的各个字符串再进行拼接
+	ret := []string{}
+	for i, s := range S {
+		// 差了第i个字符的剩余字符串往下传，并将得到的结果进行合并
+		tmp := fmt.Sprintf("%s%s", S[:i], S[i+1:])
+		fmt.Println(tmp, i, S, S[:i], S[i+1:])
+		res := permutation(tmp)
+		for _, r := range res {
+			ret = append(ret, fmt.Sprintf("%c%s", s, r))
+		}
+	}
+	return ret
+}
+
+func main() {
+	fmt.Println(permutation("123"))
 }
