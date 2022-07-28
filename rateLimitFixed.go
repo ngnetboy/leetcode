fd4f026eb125473bca7e76a46675bc333c0b5d02
ngnetboy <ngnetboy@163.com>
2022-07-28 14:19:39 +0800

    Update rateLimitFixed.go

--- rateLimitFixed.go
+++ rateLimitFixed.go
@@ -15,6 +15,62 @@ type RateLimitFix struct {
 	LastTimeStamp time.Time
 }
 
+func (r *RateLimitFix) TryAcquireWait(waitSecond int) error {
+	timer := time.NewTimer(time.Duration(waitSecond) * time.Second)
+	done := make(chan bool)
+	timeout := make(chan bool)
+
+	go func() {
+		for {
+			err := r.TryAcquire()
+			if err == nil {
+				fmt.Println("acquire success")
+				done <- true
+				return
+			}
+			time.Sleep(time.Millisecond * 100)
+			select {
+			case <-timeout:
+				fmt.Println("timeout quite acquire")
+				return
+			default:
+				fmt.Println("try it again", time.Now())
+			}
+		}
+	}()
+
+	select {
+	case <-done:
+		fmt.Println("finish the acquireWait")
+		timer.Stop()
+		return nil
+	case <-timer.C:
+		timeout <- true
+		return errors.New("timeout")
+	}
+}
+
+func testTryAcquireWait() {
+	rlf := &RateLimitFix{
+		Interval: 2,
+		Limit:    1,
+	}
+
+	err := rlf.TryAcquire()
+	if err != nil {
+		fmt.Println("acquire failed")
+		return
+	}
+	fmt.Println("start time: ", time.Now())
+	err = rlf.TryAcquireWait(5)
+	fmt.Println("end time", time.Now())
+	if err != nil {
+		fmt.Println("acquireWait failed", err)
+		return
+	}
+
+}
+
 func (r *RateLimitFix) TryAcquire() error {
 	now := time.Now()
 	elapsedTime := now.Sub(r.LastTimeStamp).Seconds()
@@ -32,7 +88,7 @@ func (r *RateLimitFix) TryAcquire() error {
 	return nil
 }
 
-func main() {
+func testTryAcquire() {
 	rlf := &RateLimitFix{
 		Interval: 2,
 		Limit:    1,
@@ -57,5 +113,8 @@ func main() {
 	}()
 
 	time.Sleep(time.Second * 20)
+}
 
+func main() {
+	testTryAcquireWait()
 }
