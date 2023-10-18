5e04ddb0de14e45eca9afd3df9ec07afbf6ab17a
ngnetboy <ngnetboy@163.com>
2023-10-18 15:22:14 +0800

    Create consumer-producer.go

---
+++ consumer-producer.go
@@ -0,0 +1,43 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	ch := make(chan int, 10)
+	var sum int64
+	wgP := sync.WaitGroup{}
+	wgC := sync.WaitGroup{}
+
+	// producer
+	for i := 0; i < 10; i++ {
+		wgP.Add(1)
+		go func(i int) {
+			defer wgP.Done()
+			ch <- i
+		}(i)
+	}
+
+	// consumer
+	for i := 0; i < 3; i++ {
+		wgC.Add(1)
+		go func(i int) {
+			defer wgC.Done()
+			fmt.Println("consumer", i)
+			for value := range ch {
+				atomic.AddInt64(&sum, int64(value))
+			}
+		}(i)
+	}
+
+	wgP.Wait()
+	close(ch)
+
+	wgC.Wait()
+	fmt.Println(sum)
+}
